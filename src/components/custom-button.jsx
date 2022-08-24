565978317de2ced5a01bb634bf83162a3015ab78
Michael Owuor <michael.owuor@gmail.com>
2022-08-25 00:25:25 +0300

    feat: second component (#9)

--- src/components/custom-button.jsx
+++ src/components/custom-button.jsx
@@ -1,6 +1,16 @@
 import { CheckCircleIcon } from '@heroicons/react/solid';
 
-const CustomButton = () => {
-	return <div></div>;
+const CustomButton = ({ text, handleCompleteThing }) => {
+	return (
+		<button
+			className="flex items-center space-x-3 rounded-md bg-teal-500 px-6 py-3 text-2xl text-slate-200 ring-teal-500 ring-offset-4 ring-offset-slate-200 transition-opacity hover:opacity-80 focus:outline-none focus-visible:ring-4 disabled:opacity-50 dark:bg-teal-500 dark:ring-teal-500 dark:ring-offset-slate-800"
+			autoFocus
+			onClick={handleCompleteThing}
+		>
+			<span className="pointer-events-none">{text}</span>
+			<CheckCircleIcon className="pointer-events-none h-12 w-12" />
+		</button>
+	);
 };
+
 export default CustomButton;
