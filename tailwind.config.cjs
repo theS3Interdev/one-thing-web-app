1cd7e1bdea7d67622685b2ff73216f0a6f178cab
Michael Owuor <michael.owuor@gmail.com>
2023-11-21 07:51:28 +0300

    chore: update dependencies

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{js,jsx,ts,tsx}'],
-	theme: {
-		fontFamily: {
-			ubuntu: 'Ubuntu, sans-serif',
-		},
-		extend: {},
-	},
-	plugins: [
-		require('@tailwindcss/forms'),
-		require('@tailwindcss/line-clamp'),
-		require('@tailwindcss/typography'),
-	],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      ubuntu: "Ubuntu, sans-serif",
+    },
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms")],
 };
